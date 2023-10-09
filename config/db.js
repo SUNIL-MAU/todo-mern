25310e08acddec3b14ff0f8e7c4049d619eb5cc5
xceloresunil <145955146+xceloresunil@users.noreply.github.com>
2023-10-09 18:51:11 +0530

    db url changes

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     const username = encodeURIComponent(process.env.MONDGODB_USER);
     const password = encodeURIComponent(process.env.PASSWORD);
-    const uri = `mongodb+srv://${username}:${password}@cluster-todo.cqkxs1p.mongodb.net`;
+    const uri = `mongodb+srv://${username}:${password}@cluster-todo.cqkxs1p.mongodb.net/todo_db`;
     const conn = await mongoose.connect(uri);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
